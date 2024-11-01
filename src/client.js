e9583be0b7e2dea2a9e406eaa6964e7e3c09362c
VincentTol <116781794+VincentTol@users.noreply.github.com>
2024-10-31 20:24:32 -0400

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const URL = "https://kafazejaobhjkesyyvra.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthZmF6ZWphb2Joamtlc3l5dnJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxODkwMDMsImV4cCI6MjAzOTc2NTAwM30.W-JKsHi5hT4mIGZ1WVt7lslTD_GUmLymH2iZLXN2pVQ";
+const API_KEY = "csdfsdfsdf";
 
 export const supabase = createClient(URL, API_KEY);
